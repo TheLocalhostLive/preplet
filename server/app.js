9a489228c316d992c0dfc3260e46affb05ae86a1
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-06-05 18:14:12 +0530

    duplicate declaration of routes removed from app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cookieparser = require("cookie-parser");
 const dotenv = require("dotenv");
+const passport = require("passport");
 dotenv.config();
 const cors = require("cors");
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
@@ -14,59 +14,33 @@ connect.on("open", () => {
   console.log("conneted to Database");
 });
 
-
 app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 let PORT = process.env.PORT || 3005;
-
 
 //Importing Auth-Routes
 
 const routingForSignup = require("./routes/signup");
 app.use("/register", routingForSignup);
 
+const routingForGooglesignUp = require("./routes/googleSignUp");
+app.use("/authenticate", routingForGooglesignUp);
 
-const routingForGooglesignUp = require('./routes/googleSignUp');
-app.use('/authenticate',routingForGooglesignUp);
+app.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-app.get('/google',
-passport.authenticate('google',{scope:[,'email','profile']}))
+const callback = require("./routes/authGooleCallback");
+app.use("/callback", callback);
 
-const callback = require('./routes/authGooleCallback')
-app.use('/callback',callback)
+const routingForLogin = require("./routes/login");
+app.use("/login", routingForLogin);
 
-
-const routingForLogin = require('./routes/login'); 
-app.use('/login',routingForLogin);
-
-const routingForDashboard = require('./routes/dashboard');
-app.use('/dashboard',routingForDashboard);
-
-const verifyEmail = require('./routes/verifyEmail');
-app.use('/verify-email',verifyEmail);
-
-const emailVerifiedPage = require('./routes/emailVerifiedPage');
-app.use('/email-verfied', emailVerifiedPage);
-
-
-const forgotPassword = require('./routes/forgetpasword');
-app.use('/forget-password',forgotPassword);
-
-const upatedPassowrd = require('./routes/updatedPassword');
-app.use('/updated-password',upatedPassowrd);
-
-const logOut = require('./routes/logout')
-app.use('/logout',logOut);
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is Running ${PORT}`);
-})
-
+const routingForDashboard = require("./routes/dashboard");
+app.use("/dashboard", routingForDashboard);
 
 const verifyEmail = require("./routes/verifyEmail");
 app.use("/verify-email", verifyEmail);
@@ -79,6 +53,9 @@ app.use("/forget-password", forgotPassword);
 
 const upatedPassowrd = require("./routes/updatedPassword");
 app.use("/updated-password", upatedPassowrd);
+
+const logOut = require("./routes/logout");
+app.use("/logout", logOut);
 
 app.listen(PORT, () => {
   console.log(`Server is Running ${PORT}`);
--- server/routes/authGooleCallback.js
+++ server/routes/authGooleCallback.js
@@ -1,28 +1,27 @@
-const router = require('express').Router()
-const passport = require('passport')
-const session = require('express-session')
-const jwt = require('jsonwebtoken')
+const router = require("express").Router();
+const passport = require("passport");
+const session = require("express-session");
+const jwt = require("jsonwebtoken");
 
+router.use(session({ secret: process.env.SECRET }));
+router.use(passport.initialize());
+router.use(passport.session());
 
-router.use(session({secret:process.env.SECRET}));
-router.use(passport.initialize())
-router.use(passport.session())
-
-router.get('/', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
+router.get(
+  "/",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
     const payload = {
       id: req.user._id,
-      isAdmin: req.user.admin
-    }
-    const token = jwt.sign(payload ,process.env.ACCESS_TOKEN_SECRET)
+      isAdmin: req.user.admin,
+    };
+    const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
 
-    res.cookie('auth-token',token).json({message:'Reh Bhai Bhai ! Logged in',isAdmin:req.user.admin});
-    console.log('call back')
-  });
+    res
+      .cookie("auth-token", token)
+      .json({ message: "Reh Bhai Bhai ! Logged in", isAdmin: req.user.admin });
+    console.log("call back");
+  }
+);
 
-
-
-   
-module.exports = router
-        
+module.exports = router;
