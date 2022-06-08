d74ce975e5ba51e551cca128d3f2bd7ac997ba90
ANT <tonmaysardar500@gmail.com>
2022-06-08 12:49:56 +0530

    Google Drive code

--- server/utils/googleDrive.js
+++ server/utils/googleDrive.js
@@ -7,35 +7,31 @@ async function uploadFiles(){
     try {
         console.log('googing')
         const auth = new google.auth.GoogleAuth({
-            keyFile:'./prepletKey.json',
+            keyFile:'./drivePreplet.json',
             scopes:['https://www.googleapis.com/auth/drive']
         })
-        console.log('Still going')
 
         const driveService = google.drive({
             version:'v3',
             auth
         })
-        console.log('Still going')
         const fileMetaData ={
-            'name':'example',
-            'parents':[process.env.GOOGLE_API_FOLDER_ID]
+            'name':'example.jpg',
+            'parent':[process.env.GOOGLE_API_FOLDER_ID ]
         }
-        console.log('Still going')
+
         const media ={
             mimeType: 'image/jpg',
             body: fs.createReadStream('./spaceman.jpg')
         }
-        console.log('Still going')
         const response = await driveService.files.create({
             resource: fileMetaData ,
             media: media,
             field :'id'
 
         })
-        console.log('Still going')
+        console.log(response.status)
         return response.data.id
-        console.log('Still going')
     } catch (error) {
         console.log(error)
     }
