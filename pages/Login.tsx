0357d92d86593fe733726bce8f9c2b1349e8524e
unknown <aarghya171200@gmail.com>
2022-06-14 14:03:43 +0530

    Login & Signup complete

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -77,64 +77,75 @@ const Login = () => {
   if (!loginStatus) {
     return (
       <>
-        <div>
-          <ToastContainer />
+      <div className=" bg-sky-200 h-screen sm:h-screen flex justify-center items-center md:flex">
+      <div className=" mx-2 bg-white my-4 flex flex-col md:flex-row items-center max-w-screen-lg overflow-hidden rounded-3xl shadow-lg w-full md:flex drop-shadow-2xl cursor-pointer">
+        <div className="flex"><img src="img/B.jpg" className="w-[100%] h-[100%]"/></div>
+        <div className="flex bg-white">
+          <ToastContainer/>
           <div>
-            <div></div>
+            <div>
             <div>
               {/* welcome */}
               <div>
-                <h1 className={"text-red-600"}>Login</h1>
+                <p className="font-mono text-2xl text-center font-bold text-blue-600 align-top animate-bounce scale-125">Login</p>
               </div>
               {/* Form */}
               <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  <input
+                <div className="text-center pt-2">
+                  <input className="pl-4 border-2"
                     placeholder="Email"
                     type="text"
                     {...register("email", { required: true })}
                   />
                   {errors.email && errors.email.type == "required" && (
-                    <div>please enter your email id</div>
+                    <div className="text-red-600 ">please enter your email id</div>
                   )}
                 </div>
-                <div>
-                  <input
+                <div className="text-center pt-4">
+                  <input className="pl-4 border-2 "
                     placeholder="password"
                     type={inputType}
                     {...register("password", { required: true, minLength: 5 })}
                   />
-                  <ToggleInputType
+                  {/* <ToggleInputType
                     type={inputType}
                     onclick={handleShowHidePassword}
                     classStyle="absolute left-[90%] top-[10px]"
-                  />
+                  /> */}
                   {errors.password && errors.password.type === "required" && (
-                    <p>please enter your password</p>
+                    <p className="text-red-600 ">please enter your password</p>
                   )}
                   {errors.password && errors.password.type === "minLength" && (
-                    <p>please enter atleast 8 characters</p>
+                    <p className="text-red-600 ">please enter atleast 8 characters</p>
                   )}
                 </div>
-
-                <div>
+                <div className="pt-4">
+                <div className="text-white bg-gradient-to-r from-purple-500 to-white rounded-2xl border-none shadow-lg w-full justify-center items-center font-bold">
                   {" "}
-                  <button>{textOnBtn}</button>
+                  <button className="w-full">{textOnBtn}</button>
+                </div>
                 </div>
               </form>
+              {/* color - from-orange-500 to-yellow-500 */}
               {/* Links */}
-              <div>OR</div>
-              <button onClick={handleGoogleLogin}>Login with Google</button>
-              <div>
+              <p className="text-center">OR</p>
+              {/* <div className="text-white bg-gradient-to-r from-gray-600 to-white rounded-2xl border-none shadow-lg w-full justify-center items-center font-bold"> */}
+               <div>
+                  <button className="w-full" onClick={handleGoogleLogin}><img src="img/Login_With_Google.png" className="w-60 h-10"/></button>
+              </div>
+              <div className="pt-4">
                 <p>
                   Don&apos;t have an account?
                   <Link href="/Signup">
-                    <a>Register</a>
+                    <a className="underline underline-offset-1">Register</a>
                   </Link>
                 </p>
               </div>
             </div>
           </div>
+         </div>
+        </div>
+        </div>
         </div>
       </>
     );
--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -81,98 +81,104 @@ const Signup = () => {
   if (!loginStatus) {
     return (
       <>
-        <div>
-          <ToastContainer />
-          <div>
-            <div></div>
-            <div>
-              {/* welcome */}
+        <div className=" bg-sky-200 h-screen sm:h-screen flex justify-center items-center md:flex">
+          <div className=" mx-2 bg-white my-4 flex flex-col md:flex-row items-center max-w-screen-lg overflow-hidden rounded-3xl shadow-lg w-full md:flex drop-shadow-2xl cursor-pointer">
+            <div className="flex"><img src="img/B.jpg" className="w-[100%] h-[100%]" /></div>
+            <div className="flex bg-white">
+              <ToastContainer />
               <div>
-                <h1>Signup</h1>
-                <p>Create an Account</p>
-              </div>
-              {/* Form */}
-              <form onSubmit={handleSubmit(onSubmit)}>
+                <div></div>
                 <div>
-                  <div className=""></div>
-                  <input
-                    placeholder="Full Name"
-                    type="text"
-                    {...register("name", { required: true })}
-                  />
-                  {errors.name && errors.name.type == "required" && (
-                    <div>please enter your name</div>
-                  )}
-                </div>
-                <div>
-                  <input
-                    placeholder="Email"
-                    type="text"
-                    {...register("email", { required: true })}
-                  />
-                  {errors.email && errors.email.type == "required" && (
-                    <div>please enter your email id</div>
-                  )}
-                </div>
-                <div>
-                  <input
-                    placeholder="password"
-                    type={inputType}
-                    {...register("password", { required: true, minLength: 5 })}
-                  />
-                  <ToggleInputType
+                  {/* welcome */}
+                  <div>
+                    <p className="font-mono text-2xl text-center font-bold text-blue-600 align-top animate-bounce scale-125">Signup</p>
+                    <p className="font-mono text-2xl text-center font-bold align-top scale-75">Create an Account</p>
+                  </div>
+                  {/* Form */}
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="text-center pt-2">
+                      <input className="pl-4 border-2"
+                        placeholder="Full Name"
+                        type="text"
+                        {...register("name", { required: true })}
+                      />
+                      {errors.name && errors.name.type == "required" && (
+                        <div className="text-red-600 ">please enter your name</div>
+                      )}
+                    </div>
+                    <div className="text-center pt-2">
+                      <input className="pl-4 border-2"
+                        placeholder="Email"
+                        type="text"
+                        {...register("email", { required: true })}
+                      />
+                      {errors.email && errors.email.type == "required" && (
+                        <div className="text-red-600 ">please enter your email id</div>
+                      )}
+                    </div>
+                    <div className="text-center pt-2">
+                      <input className="pl-4 border-2"
+                        placeholder="password"
+                        type={inputType}
+                        {...register("password", { required: true, minLength: 5 })}
+                      />
+                      {/* <ToggleInputType
                     type={inputType}
                     onclick={handleShowHidePassword}
                     classStyle="absolute left-[90%] top-[10px]"
-                  />
-                  {errors.password && errors.password.type === "required" && (
-                    <p>please enter your password</p>
-                  )}
-                  {errors.password && errors.password.type === "minLength" && (
-                    <p>please enter atleast 5 characters</p>
-                  )}
-                </div>
-                <div>
-                  <input
-                    placeholder="confirm password"
-                    type={inputTypeConfirmPass}
-                    {...register("cpassword", {
-                      required: true,
-                      minLength: 3,
-                      validate: value => {
-                        console.log(errors);
-                        return value === password || "Passwords do not match";
-                      },
-                    })}
-                  />
-                  <ToggleInputType
+                  /> */}
+                      {errors.password && errors.password.type === "required" && (
+                        <p className="text-red-600 ">please enter your password</p>
+                      )}
+                      {errors.password && errors.password.type === "minLength" && (
+                        <p className="text-red-600 ">please enter atleast 5 characters</p>
+                      )}
+                    </div>
+                    <div className="text-center pt-2">
+                      <input className="pl-4 border-2"
+                        placeholder="confirm password"
+                        type={inputTypeConfirmPass}
+                        {...register("cpassword", {
+                          required: true,
+                          minLength: 3,
+                          validate: value => {
+                            console.log(errors);
+                            return value === password || "Passwords do not match";
+                          },
+                        })}
+                      />
+                      {/* <ToggleInputType
                     type={inputTypeConfirmPass}
                     onclick={handleShowHideConfirmPass}
                     classStyle="absolute left-[90%] top-[10px]"
-                  />
+                  /> */}
 
-                  {errors.cpassword && errors.cpassword.type === "required" && (
-                    <div>please confirm your password</div>
-                  )}
-                  {errors.cpassword && errors.cpassword.type === "validate" && (
-                    <div>{errors.cpassword.message}</div>
-                  )}
+                      {errors.cpassword && errors.cpassword.type === "required" && (
+                        <div className="text-red-600 ">please confirm your password</div>
+                      )}
+                      {errors.cpassword && errors.cpassword.type === "validate" && (
+                        <div>{errors.cpassword.message}</div>
+                      )}
+                    </div>
+                    <div className="pt-2">
+                      <div className="text-white bg-gradient-to-r from-purple-500 to-white rounded-2xl border-none shadow-lg w-full justify-center items-center font-bold">
+                        {" "}
+                        <button className="w-full">{textOnBtn}</button>{" "}
+                      </div>
+                    </div>
+                  </form>
+                  {/* Links */}
+                  <p className="text-center">OR</p>
+                  <button className="w-full" onClick={handleGoogleSignup}><img src="img/Login_With_Google.png" className="w-60 h-10" /></button>
+                  <div className="text-center">
+                    <p>
+                      Already an User?
+                      <Link href="/Login">
+                        <a className="underline underline-offset-1">login</a>
+                      </Link>
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  {" "}
-                  <button>{textOnBtn}</button>{" "}
-                </div>
-              </form>
-              {/* Links */}
-              <div>OR</div>
-              <button onClick={handleGoogleSignup}>Signup with Google</button>
-              <div>
-                <p>
-                  Already an User
-                  <Link href="/Login">
-                    <a>login</a>
-                  </Link>
-                </p>
               </div>
             </div>
           </div>
