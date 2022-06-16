268f12a228e5be850e4ff0266892307281c18483
unknown <aarghya171200@gmail.com>
2022-06-16 18:00:31 +0530

    Forget password page & change password added

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,7 @@ module.exports = {
       },
       backgroundImage: {
         team: "url('/images/Team-b.png')",
+        
         // 'footer-texture': "url('/img/footer-texture.png')",
       },
     },
